f083c93191cf76bd7947b8a0d98d091a38691ab8
Aidan Fuhrer <aidanfuhrer@gmail.com>
2016-09-12 18:46:49 -0400

    Added token middleware and test route

--- src/app/routes/api.js
+++ src/app/routes/api.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-//var jwt = require('jwt');
-var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
 var config = require('../../config');
 var User = require('../models/user');
 
@@ -48,8 +47,6 @@ module.exports = function(app, express){
   var username = req.body.username;
   var password = req.body.password;
 
-  console.log('User trying to log in, username : ', username, ', password : ', password);
-
     User.findOne({'username':username}, function(error, user){
       if(error){
         res.status(200).send({success:false, message:error});
@@ -57,7 +54,13 @@ module.exports = function(app, express){
         res.status(200).send({success:false, message:'This username doesn\'t exist.'});
       } else if(user){
           if(user.comparePassword(password)){
-            res.status(200).send({success:true, message:'Welcome back'});
+            jwt.sign({username:user.username,user_id:user._id}, secret, {expiresIn:'360000'}, function(error, token){
+              if(error){
+                res.status(200).send({success:false, message:error});
+              } else {
+                res.status(200).send({success:true, message:'Welcome back', token:token});
+              }
+            });
           } else {
             res.status(200).send({success:false, message:'Incorrect password.'});
           }
@@ -65,6 +68,44 @@ module.exports = function(app, express){
     });
   });
   //////////////////////////////////////////////////////////////////////////////
+
+  //////////////////////////////////////////////////////////////////////////////
+  apiRouter.use(function(req, res, next){
+
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    console.log('coming trough the Middleware');
+
+    if(token){
+      console.log(token);
+      jwt.verify(token, secret, function(error, decoded){
+        if(error){
+          res.status(403).send({
+            success:false,
+              message:error
+          });
+        } else {
+          req.token = decoded;
+          console.log('Token is valid');
+          next();
+        }
+      });
+    } else {
+      console.log('No Token Provided');
+      res.status(403).send({
+        succes:false,
+        message:'No token provided'
+      });
+    }
+  });
+
+  //////////////////////////////////////////////////////////////////////////////
+  apiRouter.get('/updateStats', function(req, res){
+    console.log('Someone on Hola');
+    var totalViews = Math.floor(Math.random() * (1000));
+    var totalViewsChange = Math.floor(Math.random() * (100));
+    res.status(200).send({success:true,totalViews:totalViews,totalViewsChange:totalViewsChange});
+  });
+////////////////////////////////////////////////////////////////////////////////
 
   return apiRouter;
 };
