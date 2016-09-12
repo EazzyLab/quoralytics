410e3a5bc9ebb190e054d76d3ff5f82226d42ce7
Aidan Fuhrer <aidanfuhrer@gmail.com>
2016-09-12 18:14:55 -0400

    Created login route

--- src/app/routes/api.js
+++ src/app/routes/api.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+//var jwt = require('jwt');
+var bcrypt = require('bcrypt');
 var config = require('../../config');
 var User = require('../models/user');
 
@@ -32,10 +34,34 @@ module.exports = function(app, express){
         if(error){
           res.status(200).send({success:false, message:error});
         }
-        res.status(200).send({success:false, message:'Hello'});
+        res.status(200).send({success:true, message:'Hello'});
       });
     }
 
+    });
+  });
+  //////////////////////////////////////////////////////////////////////////////
+
+  //////////////////////////////////////////////////////////////////////////////
+  apiRouter.post('/login', function(req, res){
+
+  var username = req.body.username;
+  var password = req.body.password;
+
+  console.log('User trying to log in, username : ', username, ', password : ', password);
+
+    User.findOne({'username':username}, function(error, user){
+      if(error){
+        res.status(200).send({success:false, message:error});
+      } else if(!user){
+        res.status(200).send({success:false, message:'This username doesn\'t exist.'});
+      } else if(user){
+          if(user.comparePassword(password)){
+            res.status(200).send({success:true, message:'Welcome back'});
+          } else {
+            res.status(200).send({success:false, message:'Incorrect password.'});
+          }
+      }
     });
   });
   //////////////////////////////////////////////////////////////////////////////
