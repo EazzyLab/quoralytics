587184a2975dba09c9ebd0bb81ddd69dac581c64
Aidan Fuhrer <aidanfuhrer@gmail.com>
2016-09-12 21:50:38 -0400

    Added updateStats functioN

--- src/public/dashboard/dashboard.controller.js
+++ src/public/dashboard/dashboard.controller.js
@@ -9,6 +9,25 @@
 
     var vm = this;
 
+    vm.updateStats = updateStats;
 
+    initController();
+
+    function initController(){
+      updateStats()
+    }
+
+    function updateStats(){
+      QuoraService.UpdateStats()
+        .success(function(data){
+          vm.totalViews = data.totalViews;
+          vm.totalViewsChange = data.totalViewsChange;
+          if(data.success){
+            FlashService.Success('Stats freshly updated !', false);
+          } else {
+            FlashService.Error(data.message, false);
+          }
+        });
+      };
     }
 })();
