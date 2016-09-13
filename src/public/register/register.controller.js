819b239aa7ff0a37a0028fca6e3a95ce07198869
Aidan Fuhrer <aidanfuhrer@gmail.com>
2016-09-12 21:37:54 -0400

    Added registration function

--- src/public/register/register.controller.js
+++ src/public/register/register.controller.js
@@ -5,11 +5,26 @@
         .module('app')
         .controller('RegisterController', RegisterController);
 
-    RegisterController.$inject = ['$location', '$rootScope', 'FlashService'];
-    function RegisterController($location, $rootScope, FlashService) {
+    RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
+    function RegisterController(UserService, $location, $rootScope, FlashService) {
         var vm = this;
 
+        vm.register = register;
 
+        function register() {
+          console.log('registration in process');
+            vm.dataLoading = true;
+            UserService.Create(vm.user)
+                .then(function (response) {
+                    if (response.success) {
+                        FlashService.Success('Registration successful ! Please login to continue.', true);
+                        $location.path('/login');
+                    } else {
+                        FlashService.Error(response.message);
+                        vm.dataLoading = false;
+                    }
+                });
+        }
     }
-    
+
 })();
